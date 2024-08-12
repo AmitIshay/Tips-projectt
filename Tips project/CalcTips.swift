2f069cc0967bee2171141f105abf2e4093dbc777
AmitIshay <amitishay@gmail.com>
2024-08-12 20:07:11 +0300

    Commit

--- Tips project/CalcTips.swift
+++ Tips project/CalcTips.swift
@@ -7,13 +7,91 @@
 
 import UIKit
 
-class CalcTips: UIViewController {
+class CalcTips: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var totalHoursLbl: UILabel!
+    @IBOutlet weak var workerTime: UITextField!
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    var workers: [Worker] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Set the data source and delegate
+                tableView.dataSource = self
+                tableView.delegate = self
+                
+                // Register a default cell
+                tableView.register(UITableViewCell.self, forCellReuseIdentifier: "WorkerCell")
+            }
+    
+    
+    @IBAction func nextButtonView(_ sender: Any) {
     }
+    
+    @IBAction func addWorkerButton(_ sender: Any) {
+        // Ensure that both fields are not empty
+        guard let name = nameInput.text, !name.isEmpty,
+              let timeShift = workerTime.text, !timeShift.isEmpty else {
+            // Optionally show an alert to the user if fields are empty
+            let alert = UIAlertController(title: "Error", message: "Please enter both name and time shift.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        // Create a new Worker and add to the list
+        let newWorker = Worker(name: name, timeShift: timeShift)
+        workers.append(newWorker)
+        
+        // Print the new worker's details to the console
+        print("Added new worker: Name: \(newWorker.name), Time Shift: \(newWorker.timeShift)")
+        
+        // Optionally update the UI to reflect the new worker list
+        updateTotalHoursLabel()
+        tableView.reloadData() // Refresh the table view
 
-
+        // Clear text fields after adding the worker
+        nameInput.text = ""
+        workerTime.text = ""
+    }
+    func updateTotalHoursLabel() {
+        var totalMinutes = 0
+        
+        for worker in workers {
+            if let time = timeStringToMinutes(worker.timeShift) {
+                totalMinutes += time
+            }
+        }
+        
+        // Convert total minutes to hours and minutes
+        let hours = totalMinutes / 60
+        let minutes = totalMinutes % 60
+        
+        // Update the label
+        totalHoursLbl.text = String(format: "%02d:%02d", hours, minutes)
+    }
+    
+    func timeStringToMinutes(_ timeString: String) -> Int? {
+        let components = timeString.split(separator: ":")
+        if components.count == 2,
+           let hours = Int(components[0].trimmingCharacters(in: .whitespaces)),
+           let minutes = Int(components[1].trimmingCharacters(in: .whitespaces)) {
+            return hours * 60 + minutes
+        }
+        return nil
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return workers.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "WorkerCell", for: indexPath)
+            let worker = workers[indexPath.row]
+            cell.textLabel?.text = "Name: \(worker.name), Time Shift: \(worker.timeShift)"
+            return cell
+        }
+        
 }
 
--- Tips project/Worker.swift
+++ Tips project/Worker.swift
@@ -1,93 +1,13 @@
+// Worker.swift
+
 import Foundation
 
-// Define the Worker class conforming to Codable for serialization
-class Worker: Codable {
-    // Private storage properties
-        private var _workerName: String
-        private var _timeShiftWorker: String
-        private var _exchangeTimeShiftWorker: Double
-        private var _workerType: String
-        private var _totalMoneyWorker: Int
-        private var _startTimeWorker: String
-        private var _finishTimeWorker: String
-
-        // Public getters and setters
-        public var workerName: String {
-            get {
-                return _workerName
-            }
-            set {
-                _workerName = newValue
-            }
-        }
-
-        public var timeShiftWorker: String {
-            get {
-                return _timeShiftWorker
-            }
-            set {
-                _timeShiftWorker = newValue
-            }
-        }
-
-        public var exchangeTimeShiftWorker: Double {
-            get {
-                return _exchangeTimeShiftWorker
-            }
-            set {
-                _exchangeTimeShiftWorker = newValue
-            }
-        }
-
-        public var workerType: String {
-            get {
-                return _workerType
-            }
-            set {
-                _workerType = newValue
-            }
-        }
-
-        public var totalMoneyWorker: Int {
-            get {
-                return _totalMoneyWorker
-            }
-            set {
-                _totalMoneyWorker = newValue
-            }
-        }
-
-        public var startTimeWorker: String {
-            get {
-                return _startTimeWorker
-            }
-            set {
-                _startTimeWorker = newValue
-            }
-        }
-
-        public var finishTimeWorker: String {
-            get {
-                return _finishTimeWorker
-            }
-            set {
-                _finishTimeWorker = newValue
-            }
-        }
-
-        // Initializer
-        public init(workerName: String, timeShiftWorker: String, exchangeTimeShiftWorker: Double,
-                    workerType: String, totalMoneyWorker: Int, startTimeWorker: String, finishTimeWorker: String) {
-            self._workerName = workerName
-            self._timeShiftWorker = timeShiftWorker
-            self._exchangeTimeShiftWorker = exchangeTimeShiftWorker
-            self._workerType = workerType
-            self._totalMoneyWorker = totalMoneyWorker
-            self._startTimeWorker = startTimeWorker
-            self._finishTimeWorker = finishTimeWorker
-        }
-    // Implement CustomStringConvertible
-    public var description: String {
-        return "Worker(name: \(workerName), timeShift: \(timeShiftWorker), exchangeTimeShift: \(exchangeTimeShiftWorker), type: \(workerType), totalMoney: \(totalMoneyWorker), startTime: \(startTimeWorker), finishTime: \(finishTimeWorker))"
+class Worker {
+    var name: String
+    var timeShift: String
+    
+    init(name: String, timeShift: String) {
+        self.name = name
+        self.timeShift = timeShift
     }
 }
